52e932c53d68a1a2c023d7432b8bcc36d7fa862e
Gowtham kumar <kumargowtham.j@gmail.com>
2023-10-01 22:51:52 +0530

    small changes

--- src/Components/SortingVisualizer.jsx
+++ src/Components/SortingVisualizer.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
-import Bars from "./Bars";
+import { useState, useEffect, useRef } from "react";
 import bubbleSort from "../algorithms/bubblesort";
 import Alert from "./alert";
 import insertionSort from "../algorithms/insertionsort";
 import { shuffle, swap } from "../algorithms/helper";
 
-const DELAY = 100
+const DELAY = 10
+const ANIME_DELAY = 10
 const BAR_COUNT = 50
 
 function SortingVisualizer() {
     const [arr, setArr] = useState([])
     const [isSorting, setIsSorting] = useState(false)
     const [showAlert, setShowAlert] = useState(false)
-
+    const barsRef = useRef(null)
     useEffect(initializeArray, [])
 
     function initializeArray() {
@@ -26,10 +26,6 @@ function SortingVisualizer() {
     }
 
     function showSorting(swaps) {
-        if (isSorting) {
-            showAlertMessage()
-            return
-        }
         setIsSorting(true)
         swaps.forEach((swapPair, index) => {
             setTimeout(() => {
@@ -47,15 +43,37 @@ function SortingVisualizer() {
     }
 
     function sortingCompleted() {
+        animateSortedBars()
         setIsSorting(false)
     }
 
+    function animateSortedBars() {
+        const bars = Array.from(barsRef.current.children)
+        for(let i = 0; i < bars.length; i++) {
+            const bar = bars[i]
+            setTimeout(() => {
+                bar.classList.add('bg-teal-800')
+            }, i * ANIME_DELAY )
+            setTimeout(() => {
+                bar.classList.remove('bg-teal-800')
+            }, i * ANIME_DELAY * 2 )
+        }
+    }
+
     function showBubbleSort() {
+        if (isSorting) {
+            showAlertMessage()
+            return
+        }
         const swaps = bubbleSort(arr)
         showSorting(swaps)
     }
 
     function showInsertionSort() {
+        if (isSorting) {
+            showAlertMessage()
+            return
+        }
         const swaps = insertionSort(arr)
         showSorting(swaps)
     }
@@ -64,16 +82,22 @@ function SortingVisualizer() {
         setShowAlert(true)
         setTimeout(() => {
             setShowAlert(false)
-        }, 3000);
+        }, 4000);
     }
 
     return (
-        <div className="h-full w-full flex flex-col lg:flex-row-reverse border-2 bg-teal-100 border-teal-900 ">
+        <div className="h-full w-full flex flex-col lg:flex-row-reverse border-2 bg-teal-100 border-teal-900">
             <div className="lg:w-4/5 w-full h-full p-4">
-                <Bars array={arr} />
+                <div className="h-full w-full flex gap-1 items-end" ref={barsRef}>
+                    {arr.map((number, index) => {
+                        return <div key={index} className={`w-6 rounded-sm bg-teal-500`} style={{ height: `${number * 10}px` }}></div>
+                    })}
+                </div>
             </div>
-            <div className="lg:w-1/5 flex justify-center lg:min-w-12 lg:flex-col gap-4 p-2 bg-teal-500 border-2 border-t-4 lg:border-r-4 lg:border-t-2 border-teal-900 overflow-clip">
-                <button onClick={initializeArray} className="p-2 border-2 rounded font-bold bg-teal-100 text-teal-900 border-t-4 border-teal-900">Suffle Array</button>
+            <div className="lg:w-1/5 flex justify-center items-stretch lg:flex-col gap-4 p-2 overflow-x-scroll space-x-4 lg:overflow-x-hidden bg-teal-500 border-2 border-t-4 lg:border-r-4 lg:border-t-2 border-teal-900">
+                <button onClick={initializeArray} className="p-2 border-2 rounded font-bold bg-teal-100 text-teal-900 border-t-4 border-teal-900">SuffleArray</button>
+                <button onClick={showInsertionSort} className="p-2 border-2 rounded font-bold bg-teal-100 text-teal-900 border-t-4 border-teal-900">QuickSort</button>
+                <button onClick={showInsertionSort} className="p-2 border-2 rounded font-bold bg-teal-100 text-teal-900 border-t-4 border-teal-900">MergeSort</button>
                 <button onClick={showBubbleSort} className="p-2 border-2 rounded font-bold bg-teal-100 text-teal-900 border-t-4 border-teal-900">BubbleSort</button>
                 <button onClick={showInsertionSort} className="p-2 border-2 rounded font-bold bg-teal-100 text-teal-900 border-t-4 border-teal-900">InsertionSort</button>
             </div>
--- src/algorithms/bubblesort.js
+++ src/algorithms/bubblesort.js
@@ -1,3 +1,5 @@
+import { swap } from "./helper"
+
 export default function bubbleSort(array) {
     const swaps = []
     const arr = [...array]
@@ -5,9 +7,7 @@ export default function bubbleSort(array) {
         for (var j = 0; j < (arr.length - i - 1); j++) {
             if (arr[j] > arr[j + 1]) {
                 swaps.push([j,j+1])
-                const temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+                swap(arr, j, j+1)
             }
         }
     }
